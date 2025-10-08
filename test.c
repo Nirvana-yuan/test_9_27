6d8ab334ba90e8b094b796f43f95bdcb72b8017d
Nirvana-yuan <sny070810@163.com>
2025-10-08 11:45:52 +0800

    c

--- test.c
+++ test.c
@@ -6,7 +6,7 @@ int main()
 	printf("%zu\n", sizeof(int));
 	int len = strlen("abc");
 	printf("%d\n",len);
-	char arr1[] = "abcd";
+	char arr1[] = "abcde";
 	char arr2[] = { 'a','b','c','d','\0' };
 	printf(arr1);
 	printf(arr2);
