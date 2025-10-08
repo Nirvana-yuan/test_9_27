9c4e39d2297f732d70ecd3a57dc32c2692d0f990
Nirvana-yuan <sny070810@163.com>
2025-10-08 11:38:34 +0800

    C学习

--- test.c
+++ test.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
 	printf("hello,C\n");
-	printf("hello,C\n");
-	printf("hello,C\n");
-	printf("hello,C\n");
+	printf("%zu\n", sizeof(int));
+	int len = strlen("abc");
+	printf("%d\n",len);
+	char arr1[] = "abcd";
+	char arr2[] = { 'a','b','c','d','\0' };
+	printf(arr1);
 	return 0;
 }
